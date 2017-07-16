829f0b650aab9282c1492f53a3b8b640cf49aeac
Richard <rgbailey@gmail.com>
2017-07-16 16:22:05 -0700

    tweeks for using an adafruit circuit playground
    tweeks to use an Adafruit Circuit Playground for testing

--- compass_test_v0_0_1.ino
+++ compass_test_v0_0_1.ino
@@ -5,86 +5,90 @@
 
 #define NUM_LEDS 1
 // data piin that led data will be written out over
-#define DATA_PIN 6
+#define DATA_PIN 17
 
 LSM303 compass;
 CRGB leds[NUM_LEDS];
 
 void setup() {
-  // sanity check delay - allows reprogramming if accidently blowing power w/leds
-  delay(2000);
-  Serial.begin(115200);
-  Wire.begin();
-  compass.init();
-  compass.enableDefault();
-  /*
+	// sanity check delay - allows reprogramming if accidently blowing power w/leds
+	delay(2000);
+	Serial.begin(115200);
+	Wire.begin();
+	compass.init();
+	compass.enableDefault();
+	/*
 	Calibration values; the default values of +/-32767 for each axis
-   	lead to an assumed magnetometer bias of 0. Use the Calibrate example
-   	program to determine appropriate values for your particular unit.
-   	*/
-  compass.m_min = (LSM303::vector<int16_t>){
-    -1162, -1032, -491    };
-  compass.m_max = (LSM303::vector<int16_t>){
-    +628, +347, +526      };
-  // setup neopixels
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  fill_solid ( &(leds[0]), NUM_LEDS, CRGB::Black);
-  FastLED.show();
-  delay(1000);
-  FastLED.setBrightness(96);
+	lead to an assumed magnetometer bias of 0. Use the Calibrate example
+	program to determine appropriate values for your particular unit.
+	*/
+	compass.m_min = (LSM303::vector<int16_t>)
+	{
+		-417, -942, -178
+	};
+	compass.m_max = (LSM303::vector<int16_t>)
+	{
+		+663, +152, +815
+	};
+	// setup neopixels
+	FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+	fill_solid ( &(leds[0]), NUM_LEDS, CRGB::Black);
+	FastLED.show();
+	delay(1000);
+	FastLED.setBrightness(96);
 }
 void loop(void)
 {
-  /* Get a new sensor event */
-  compass.read();
-  double heading = compass.heading();
-  float roll = atan(compass.a.y/sqrt(pow(compass.a.z,2)+pow(compass.a.z,2)))*(180/PI);
-  float pitch = atan(compass.a.x/sqrt(pow(compass.a.y,2)+pow(compass.a.z,2)))*(180/PI);
+	/* Get a new sensor event */
+	compass.read();
+	double heading = compass.heading();
+	float roll = atan(compass.a.y/sqrt(pow(compass.a.z,2)+pow(compass.a.z,2)))*(180/PI);
+	float pitch = atan(compass.a.x/sqrt(pow(compass.a.y,2)+pow(compass.a.z,2)))*(180/PI);
 
-  Serial.print("X: "); 
-  Serial.print(compass.m.x); 
-  Serial.print("  ");
-  Serial.print("Y: "); 
-  Serial.print(compass.m.y); 
-  Serial.print("  ");
-  Serial.print("Z: "); 
-  Serial.println(compass.m.z);
+	Serial.print("X: ");
+	Serial.print(compass.m.x);
+	Serial.print("  ");
+	Serial.print("Y: ");
+	Serial.print(compass.m.y);
+	Serial.print("  ");
+	Serial.print("Z: ");
+	Serial.println(compass.m.z);
 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); 
-  Serial.print(compass.a.x); 
-  Serial.print("  ");
-  Serial.print("Y: "); 
-  Serial.print(compass.a.y); 
-  Serial.print("  ");
-  Serial.print("Z: "); 
-  Serial.println(compass.a.z);
-  /* Display calculated results */
-  Serial.print("Pitch: ");
-  Serial.print(pitch);
-  Serial.print(" Roll: ");
-  Serial.print(roll);
-  Serial.print(" Compass Heading: ");
-  Serial.println(compass.heading());
+	/* Display the results (acceleration is measured in m/s^2) */
+	Serial.print("X: ");
+	Serial.print(compass.a.x);
+	Serial.print("  ");
+	Serial.print("Y: ");
+	Serial.print(compass.a.y);
+	Serial.print("  ");
+	Serial.print("Z: ");
+	Serial.println(compass.a.z);
+	/* Display calculated results */
+	Serial.print("Pitch: ");
+	Serial.print(pitch);
+	Serial.print(" Roll: ");
+	Serial.print(roll);
+	Serial.print(" Compass Heading: ");
+	Serial.println(compass.heading());
 
-  if (heading >=315 && heading <360){
-    leds[0] = CRGB::Green;
-  }
-  if (heading >= 0 && heading <45) {
-    leds[0] = CRGB::Green;
-  }
-  if (heading >=45 && heading <135) {
-    leds[0] = CRGB::Yellow;
-  }
-  if (heading >=135 && heading <225) {
-    leds[0] = CRGB::Red;
-  }
-  if (heading >=225 && heading <315) {
-    leds[0] = CRGB::Blue;
-  }
+	if (heading >=315 && heading <360){
+		leds[0] = CRGB::Green;
+	}
+	if (heading >= 0 && heading <45) {
+		leds[0] = CRGB::Green;
+	}
+	if (heading >=45 && heading <135) {
+		leds[0] = CRGB::Yellow;
+	}
+	if (heading >=135 && heading <225) {
+		leds[0] = CRGB::Red;
+	}
+	if (heading >=225 && heading <315) {
+		leds[0] = CRGB::Blue;
+	}
 
-  FastLED.show();
-  FastLED.delay(250);
+	FastLED.show();
+	FastLED.delay(250);
 }
 
 
